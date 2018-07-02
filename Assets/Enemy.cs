220ab3642d19ff6e1dcfb16f0cc3b99fd4555705
rollerbot <lochiejohnson@y7mail.com>
2018-07-02 18:10:03 +1000

    enemys are now able to shoot at the player

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,16 +4,53 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    float shotTimer = 5;
+    int bulletSpeed = 5;
+    
+
+    Transform player;
+    Transform firePoint;
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        //get player referance
+        player = GameObject.FindWithTag("Player").transform;
+
+        //get fire point referance
+        firePoint = transform.Find("firePoint");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (shotTimer <= 0)
+        {
+            //shoot
+            Shoot();
 
-   
+            //reset shot timer
+            shotTimer = Random.Range(1f, 4f);
+        }
+
+        //reduce timer
+        shotTimer -= Time.deltaTime;
+
+    }
+
+   void Shoot()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            Resources.Load("bulletPrefab") as GameObject,
+            firePoint.position,
+           firePoint.rotation);
+
+        //rotate bullet towards player
+        bullet.transform.LookAt(player);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+    }
 
 }
